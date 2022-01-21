e389cb899096834ca155b7a8f73ffeb49aa15739
erkantekel <erkanntekel@gmail.com>
2022-01-21 19:42:13 +0300

    first commit

---
+++ src/main/java/com/example/patikaProject/PatikaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.patikaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatikaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/patikaProject/PatikaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patikaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
